240dd00765ad09c33869ac0657067f3faea17c22
mukul <mukulraw199517@gmail.com>
2019-09-27 06:26:51 +0530

    started

--- app/src/main/java/com/mrtecks/grocery/Spalsh.java
+++ app/src/main/java/com/mrtecks/grocery/Spalsh.java
@@ -2,13 +2,33 @@ package com.mrtecks.grocery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Spalsh extends AppCompatActivity {
+
+    Timer t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spalsh);
+
+        t = new Timer();
+
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(Spalsh.this , Login.class);
+                startActivity(intent);
+                finish();
+
+            }
+        } , 1200);
+
     }
 }
